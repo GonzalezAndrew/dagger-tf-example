8c8c490a5d4aaeba555ac69a9a1a86eeb2b58854
GonzalezAndrew <andrewgonzo23@gmail.com>
2023-01-10 20:21:08 -0600

    Changing publish workflow to a scheduled job that will run everyday. Adding two terraform examples, one which will just use dagger to build a docker image and another which will deploy an AWS lambda function.

--- ci/main.py
+++ ci/main.py
@@ -1,65 +1,68 @@
 """
-Create a multi-build pipeline for a Go application.
+Create a simple pipeline for a deploying a Lambda function.
 """
 
-
-import itertools
+import os
 import sys
 
 import anyio
+import dagger
 import graphql
 
-import dagger
+
+def build_image(client: dagger.Client):
+    """ Returns a build environment to be used by a Terraform project.
+    :param client: The dagger Client object.
+    :return: A dagger Container object.
+    :rtype: dagger.Container
+    """
+    # get reference to the local project
+    src = client.host().directory(".")
+
+    # configure the secrets for the build container
+    aws_access_key_id = client.host().env_variable("AWS_ACCESS_KEY_ID").secret()
+    aws_secret_access_key = client.host().env_variable("AWS_SECRET_ACCESS_KEY").secret()
+    aws_session_token = client.host().env_variable("AWS_SESSION_TOKEN").secret()
+    aws_region = os.getenv("AWS_REGION", "us-east-1")
+
+    # to run docker in docker, we need to connect to the docker socket.
+    docker_host = client.host().unix_socket("/var/run/docker.sock")
+
+    return (
+        client.container()
+        .from_("ghcr.io/gonzalezandrew/dagger-tf-example:main")
+        .with_env_variable("AWS_REGION", aws_region)
+        .with_secret_variable("AWS_ACCESS_KEY_ID", aws_access_key_id)
+        .with_secret_variable("AWS_SECRET_ACCESS_KEY", aws_secret_access_key)
+        .with_secret_variable("AWS_SESSION_TOKEN", aws_session_token)
+        .with_unix_socket("/var/run/docker.sock", docker_host)
+        .with_mounted_directory("/src", src)
+        .with_workdir("/src")
+    )
 
 
+def init(image: dagger.Container):
+    pass
 
-async def build():
-    print("Building with Dagger")
+def plan(image: dagger.Container):
+    pass
 
-    # define build matrix
-    oses = ["linux", "darwin"]
-    arches = ["amd64", "arm64"]
+def apply(image: dagger.Container):
+    pass
 
+async def main():
+    """main"""
     # initialize dagger client
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
 
-        # get reference to the local project
-        src = client.host().directory(".")
-
-        # create empty directory to put build outputs
-        outputs = client.directory()
-
-        golang = (
-            # get `golang` image
-            client.container()
-            .from_("golang:latest")
-            # mount source code into `golang` image
-            .with_mounted_directory("/src", src)
-            .with_workdir("/src")
-        )
-
-        for goos, goarch in itertools.product(oses, arches):
-            # create a directory for each OS and architecture
-            path = f"build/{goos}/{goarch}/"
-
-            build = (
-                golang
-                # set GOARCH and GOOS in the build environment
-                .with_env_variable("GOOS", goos)
-                .with_env_variable("GOARCH", goarch)
-                .with_exec(["go", "build", "-o", path])
-            )
-
-            # add build to outputs
-            outputs = outputs.with_directory(path, build.directory(path))
-
-        # write build artifacts to host
-        await outputs.export(".")
-
+        # lets grab the build environment
+        build = build_image(client=client)
+        
+        await build.exec(["terraform", "-version"]).stdout()
 
 if __name__ == "__main__":
     try:
-        anyio.run(build)
+        anyio.run(main)
     except graphql.GraphQLError as e:
         print(e.message, file=sys.stderr)
         sys.exit(1)
---
+++ docker_build/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.1.1"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_registry_image" "helloworld" {
+  name = "helloworld:1.0"
+
+  build {
+    context = "${path.cwd}/context"
+  }
+}
